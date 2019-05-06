3802dbec4c2c4b9d7e69445421539dd34f967513
lenoil98 <hamiltcl@verizon.net>
2019-05-06 14:25:39 -0400

    Update vm_version_ppc.cpp

--- src/hotspot/cpu/ppc/vm_version_ppc.cpp
+++ src/hotspot/cpu/ppc/vm_version_ppc.cpp
@@ -37,7 +37,9 @@
 #include "utilities/globalDefinitions.hpp"
 #include "vm_version_ppc.hpp"
 
-#include <sys/sysinfo.h>
+#ifndef __FreeBSD__
+ #include <sys/sysinfo.h>
+#endif
 
 #if defined(LINUX) && defined(VM_LITTLE_ENDIAN)
 #include <sys/auxv.h>
